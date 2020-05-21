26d1948233d93787f76c505be262862b6d0cfb47
Taylor Barnes <taylor.a.barnes@gmail.com>
2020-05-21 16:10:11 -0400

    Update tutorial.sh

--- .travis/tutorial.sh
+++ .travis/tutorial.sh
@@ -5,9 +5,14 @@ echo "Tutorial"
 make_pr() {
   git config --global user.email "travis@travis-ci.com"
   git config --global user.name "Travis CI"
+  git checkout -b travis
+  git submodule update --remote
+  git add -u
+  git commit -m "Submodule update from Travis build $TRAVIS_BUILD_NUMBER"
+  #git push origin travis
   echo "Making a PR"
 }
 
-git submodule update --remote
+#git submodule update --remote
 
 make_pr
