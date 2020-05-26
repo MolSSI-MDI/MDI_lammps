12618a9885299135f652b9dd0568c960f7ce5776
Taylor Barnes <taylor.a.barnes@gmail.com>
2020-05-26 16:06:57 -0400

    Update tutorial.sh

--- .travis/tutorial.sh
+++ .travis/tutorial.sh
@@ -15,11 +15,11 @@ reset_tutorial() {
 step_config() {
     git remote -v
     if git push -v > /dev/null 2>&1 ; then
-	echo "Success: Able to push to remote."
-	cp ./.travis/badges/-working-success.svg ./.travis/dynamic_badges/step_config.svg
+        echo "Success: Able to push to remote."
+        cp ./.travis/badges/-working-success.svg ./.travis/dynamic_badges/step_config.svg
     else
-	echo "Error: Unable to push to remote.  The repo has not been configured correctly."
-	exit 1
+        echo "Error: Unable to push to remote.  The repo has not been configured correctly."
+        exit 1
     fi
     echo "================================================================="
     git status
@@ -42,8 +42,8 @@ git config --global user.email "travis@travis-ci.com"
 git config --global user.name "Travis CI"
 
 # Switch the remote to use ssh
-#git remote -v
-#git remote set-url origin git@github.com:MolSSI-MDI/MDI_lammps2.git
+git remote -v
+git remote set-url origin git@github.com:MolSSI-MDI/MDI_lammps2.git
 git checkout ${TRAVIS_BRANCH}
 
 # Reset the tutorial
