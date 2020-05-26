1e6ad0fa4fac66fe6b291b351882e8297d363917
Taylor Barnes <taylor.a.barnes@gmail.com>
2020-05-26 15:52:59 -0400

    Update tutorial.sh

--- .travis/tutorial.sh
+++ .travis/tutorial.sh
@@ -10,8 +10,23 @@ reset_tutorial() {
     cp ./.travis/badges/-failing-red.svg ./.travis/dynamic_badges/step_mdi_link.svg
     cp ./.travis/badges/-failing-red.svg ./.travis/dynamic_badges/step_mdi_nodes.svg
     cp ./.travis/badges/-failing-red.svg ./.travis/dynamic_badges/step_min_engine.svg
-    mkdir test
-    mkdir test
+}
+
+step_config() {
+    git remote -v
+    echo "================================================================="
+    git status
+    echo "================================================================="
+    #git pull
+    if git push -v ; then
+	echo "AAAAA PUSH WORKED"
+	cp ./.travis/badges/-working-success.svg ./.travis/dynamic_badges/step_config.svg
+    else
+	echo "BBBBB PUSH FAILED"
+    fi
+    #git remote add origin-pages https://${mdi_key}@github.com/MolSSI-MDI/MDI_lammps2.git > /dev/null 2>&1
+    #git push --quiet --set-upstream origin-pages travis > /dev/null 2>&1
+    #git push origin travis
 }
 
 # Write out the MDI key
@@ -22,33 +37,20 @@ echo "Host github.com" > ~/.ssh/config
 echo "  IdentityFile  $(pwd)/travis_key" >> ~/.ssh/config
 chmod 400 travis_key
 
+# Configure Travis to use Git
 git config --global user.email "travis@travis-ci.com"
 git config --global user.name "Travis CI"
 
 # Switch the remote to use ssh
 git remote -v
 git remote set-url origin git@github.com:MolSSI-MDI/MDI_lammps2.git
-
-echo "After change remote: "
 git checkout ${TRAVIS_BRANCH}
 
-# Set all of the step badges to "failed"
+# Reset the tutorial
 reset_tutorial
 
-git remote -v
-echo "================================================================="
-git status
-echo "================================================================="
-#git pull
-if git push -v ; then
-    echo "AAAAA PUSH WORKED"
-    cp ./.travis/badges/-working-success.svg ./.travis/dynamic_badges/step_config.svg
-else
-    echo "BBBBB PUSH FAILED"
-fi
-#git remote add origin-pages https://${mdi_key}@github.com/MolSSI-MDI/MDI_lammps2.git > /dev/null 2>&1
-#git push --quiet --set-upstream origin-pages travis > /dev/null 2>&1
-#git push origin travis
+# Test whether the repo has been correctly configured
+step_config
 
 config_travis() {
   echo "Making a PR"
