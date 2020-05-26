b0633675c29ed37515df0881a7dd047b3e7e3720
Taylor Barnes <taylor.a.barnes@gmail.com>
2020-05-26 15:28:04 -0400

    Update tutorial.sh

--- .travis/tutorial.sh
+++ .travis/tutorial.sh
@@ -15,8 +15,8 @@ git config --global user.email "travis@travis-ci.com"
 git config --global user.name "Travis CI"
 
 # Switch the remote to use ssh
-#git remote -v
-#git remote set-url origin git@github.com:MolSSI-MDI/MDI_lammps2.git
+git remote -v
+git remote set-url origin git@github.com:MolSSI-MDI/MDI_lammps2.git
 
 echo "After change remote: "
 git checkout ${TRAVIS_BRANCH}
