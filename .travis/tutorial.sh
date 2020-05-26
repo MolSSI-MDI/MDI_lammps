8196d0ba38fc8ed74d2388c1fd05ab1117b0ebdf
Taylor Barnes <taylor.a.barnes@gmail.com>
2020-05-26 15:06:08 -0400

    Update tutorial.sh

--- .travis/tutorial.sh
+++ .travis/tutorial.sh
@@ -2,38 +2,39 @@
 
 echo "Tutorial"
 
+# Write out the MDI key
+echo "-----BEGIN OPENSSH PRIVATE KEY-----" > travis_key
+echo ${mdi_key} >> travis_key
+echo "-----END OPENSSH PRIVATE KEY-----" >> travis_key
+echo "Host github.com" > ~/.ssh/config
+echo "  IdentityFile  $(pwd)/travis_key" >> ~/.ssh/config
+chmod 400 travis_key
+
+git config --global user.email "travis@travis-ci.com"
+git config --global user.name "Travis CI"
+
+# Switch the remote to use ssh
+#git remote -v
+#git remote set-url origin git@github.com:MolSSI-MDI/MDI_lammps2.git
+
+echo "After change remote: "
+git checkout ${TRAVIS_BRANCH}
+git remote -v
+echo "================================================================="
+git status
+echo "================================================================="
+git pull
+git push -v
+if [ "$?" = "0" ] ; then
+    echo "AAAAA PUSH WORKED"
+else
+    echo "BBBBB PUSH FAILED"
+fi
+#git remote add origin-pages https://${mdi_key}@github.com/MolSSI-MDI/MDI_lammps2.git > /dev/null 2>&1
+#git push --quiet --set-upstream origin-pages travis > /dev/null 2>&1
+#git push origin travis
+
 config_travis() {
-  git config --global user.email "travis@travis-ci.com"
-  git config --global user.name "Travis CI"
-  git checkout ${TRAVIS_BRANCH}
-
-  # Write out the MDI key
-  echo "-----BEGIN OPENSSH PRIVATE KEY-----" > travis_key
-  echo ${mdi_key} >> travis_key
-  echo "-----END OPENSSH PRIVATE KEY-----" >> travis_key
-  echo "Host github.com" > ~/.ssh/config
-  echo "  IdentityFile  $(pwd)/travis_key" >> ~/.ssh/config
-  chmod 400 travis_key
-
-  # Switch the remote to use ssh
-  #git remote -v
-  #git remote set-url origin git@github.com:MolSSI-MDI/MDI_lammps2.git
-
-  echo "After change remote: "
-  git remote -v
-  echo "================================================================="
-  git status
-  echo "================================================================="
-  git pull
-  git push -v
-  if [ "$?" = "0" ; then
-      echo "AAAAA PUSH WORKED"
-  else
-      echo "BBBBB PUSH FAILED"
-  fi
-  #git remote add origin-pages https://${mdi_key}@github.com/MolSSI-MDI/MDI_lammps2.git > /dev/null 2>&1
-  #git push --quiet --set-upstream origin-pages travis > /dev/null 2>&1
-  #git push origin travis
   echo "Making a PR"
 }
 
