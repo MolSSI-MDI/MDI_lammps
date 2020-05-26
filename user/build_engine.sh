d743a922292c1d17f77db68d5ab382074a80b9e8
Taylor Barnes <taylor.a.barnes@gmail.com>
2020-05-26 17:00:27 -0400

    Update build script

--- user/build_engine.sh
+++ user/build_engine.sh
@@ -1,3 +1,4 @@
+# Obtain dependencies
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     sudo apt-get install gfortran
     sudo apt-get install mpich libmpich-dev
@@ -12,3 +13,33 @@ fi
 export FC=gfortran
 sudo pip install -U pytest pytest-cov
 
+# Obtain a clone of LAMMPS
+git clone https://github.com/MolSSI-MDI/lammps.git
+LAMMPS_INSTALL='serial'
+
+# Configure LAMMPS
+cd lammps
+git checkout mdi
+cd src
+make yes-standard
+make no-gpu
+make no-kim
+make no-kokkos
+make no-kspace
+make no-latte
+make no-meam
+make no-mpiio
+make no-mscg
+make no-poems
+make no-python
+make no-reax
+make no-voronoi
+make no-user-qmmm
+make yes-user-mdi
+cd ../lib/mdi
+python Install.py -m gcc
+cd ../../src
+  
+# Build LAMMPS
+if test "${LAMMPS_INSTALL}" = 'serial'; then make mpi-stubs; fi
+make -j 4 "${LAMMPS_INSTALL}"
