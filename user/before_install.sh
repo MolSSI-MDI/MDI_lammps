e691eb9516781e2506a1eddb4b31558e45b91144
Taylor Barnes <taylor.a.barnes@gmail.com>
2020-05-26 16:25:50 -0400

    Add before_install.sh script

---
+++ user/before_install.sh
@@ -0,0 +1,13 @@
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+    sudo apt-get install gfortran
+    sudo apt-get install mpich libmpich-dev
+    sudo pip install mpi4py
+    sudo pip install numpy
+elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    brew update > /dev/null
+    if [ "$MPI" != "" ]; then
+        brew install $MPI
+    fi
+fi
+export FC=gfortran
+sudo pip install -U pytest pytest-cov
