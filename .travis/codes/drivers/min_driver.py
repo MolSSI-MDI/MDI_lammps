81c4bb7cfb828f69bdbe67922c45fa9d61128dc7
Taylor Barnes <taylor.a.barnes@gmail.com>
2020-05-28 16:50:39 -0400

    Add mdi test

---
+++ .travis/codes/drivers/min_driver.py
@@ -0,0 +1,13 @@
+import mdi
+
+mdi.MDI_Init(sys.argv[2],mpi_world)
+
+# Connect to the engine
+comm = mdi.MDI_Accept_Communicator()
+
+mdi.MDI_Send_Command("<NAME", comm)
+name = mdi.MDI_Recv(mdi.MDI_NAME_LENGTH, mdi.MDI_CHAR, comm)
+
+print(" Engine name: " + str(name))
+
+mdi.MDI_Send_Command("EXIT", comm)
--- .travis/codes/min_test.py
+++ .travis/codes/min_test.py
@@ -15,6 +15,9 @@ os.system("echo $USER_PATH")
 os.system("echo $BASE_PATH")
 os.system("pwd")
 
+# Run LAMMPS by itself
+#os.system("${USER_PATH}/lammps/src/lmp_mdi -in lammps.in > lammps.out")
+
 #driver_proc = subprocess.Popen([driver_name, "-mdi", "-role DRIVER -name driver -method TCP -port 8021"],
 #                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 #engine_proc = subprocess.Popen([engine_name, "-mdi", "-role ENGINE -name MM -method TCP -port 8021 -hostname localhost"])
@@ -23,6 +26,13 @@ os.system("pwd")
 #driver_tup = driver_proc.communicate()
 #engine_proc.communicate()
 
-os.system("${USER_PATH}/lammps/src/lmp_mdi -in lammps.in > lammps.out")
+# Launch the driver in the background
+driver_proc = subprocess.Popen([sys.executable, "./drivers/min_driver.py", "-mdi", "-role DRIVER -name driver -method TCP -port 8021"],
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=build_dir)
+
+# Run LAMMPS as an engine
+os.system("${USER_PATH}/lammps/src/lmp_mdi -mdi \"-role ENGINE -name TESTCODE -method TCP -port 8021 -hostname localhost\" -in lammps.in > lammps.out")
+
+driver_tup = driver_proc.communicate()
 
 print("Hello World!")
