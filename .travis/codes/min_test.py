4c1f98ae1e8611dca2f50d26d25a22041d7ed271
Taylor Barnes <taylor.a.barnes@gmail.com>
2020-05-29 10:46:54 -0400

    Update min_test.py

--- .travis/codes/min_test.py
+++ .travis/codes/min_test.py
@@ -28,13 +28,14 @@ os.system("pwd")
 print("Launching the driver")
 driver_proc = subprocess.Popen([sys.executable, "min_driver.py", "-mdi", "-role DRIVER -name driver -method TCP -port 8021"],
                                stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd="./drivers")
-driver_tup = driver_proc.communicate()
 
 # Run LAMMPS as an engine
 os.system("cp -r " + str(working_dir) + " _work")
 os.chdir("./_work")
 print("Launching the engine")
 os.system("${USER_PATH}/lammps/src/lmp_mdi -mdi \"-role ENGINE -name TESTCODE -method TCP -port 8021 -hostname localhost\" -in lammps.in > lammps.out")
+
+driver_tup = driver_proc.communicate()
 
 
 print("Hello World!")
