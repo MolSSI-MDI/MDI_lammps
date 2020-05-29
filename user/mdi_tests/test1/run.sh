f79471fcecd854ceda758c982a55ebe729ffa461
Taylor Barnes <taylor.a.barnes@gmail.com>
2020-05-29 10:36:20 -0400

    Add mdi_tests

---
+++ user/mdi_tests/test1/run.sh
@@ -0,0 +1,24 @@
+#location of required codes
+#LAMMPS_LOC=$(cat ../../../locations/LAMMPS)
+#LAMMPS_LOC=../../../lammps/src/lmp_mdi
+LAMMPS_LOC="${USER_PATH}/lammps/src/lmp_mdi"
+echo "IN run.sh:"
+echo "USER PATH: "
+echo ${USER_PATH}
+echo "LAMMPS_LOC: "
+echo ${LAMMPS_LOC}
+echo "Checking for file: "
+ls ${LAMMPS_LOC}
+echo "Working directory: "
+pwd
+ls
+
+
+#set the number of threads
+#export OMP_NUM_THREADS=1
+
+#launch LAMMPS
+${LAMMPS_LOC} -mdi "-role ENGINE -name MM -method TCP -port 8021 -hostname localhost" -in lammps.in > lammps.out
+
+echo "Test output: "
+cat lammps.out
