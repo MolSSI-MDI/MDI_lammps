163acc8f7b4df5fd6b49e28923a1c5232187ea80
Taylor Barnes <taylor.a.barnes@gmail.com>
2020-05-29 11:50:32 -0400

    Update min_test.py

--- .travis/codes/min_test.py
+++ .travis/codes/min_test.py
@@ -1,43 +1,36 @@
 import mdi
-
 import os
 import sys
 import subprocess
 
-print("MDI_INT: " + str(mdi.MDI_INT))
+def format_return(input_string):
+    my_string = input_string.decode('utf-8')
+
+    # remove any \r special characters, which sometimes are added on Windows
+    my_string = my_string.replace('\r','')
+
+    return my_string
 
 working_dir = "../../user/mdi_tests/test1"
 
 engine_name = "${USER_PATH}/lammps/src/lmp_mdi"
 
-os.system("ls ${USER_PATH}/lammps/src/lmp_mdi")
-os.system("echo $USER_PATH")
-os.system("echo $BASE_PATH")
-os.system("pwd")
-
-# Run LAMMPS by itself
-#os.system("${USER_PATH}/lammps/src/lmp_mdi -in lammps.in > lammps.out")
-
-#driver_proc = subprocess.Popen([driver_name, "-mdi", "-role DRIVER -name driver -method TCP -port 8021"],
-#                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#engine_proc = subprocess.Popen([engine_name, "-mdi", "-role ENGINE -name MM -method TCP -port 8021 -hostname localhost"])
-#engine_proc = subprocess.Popen([engine_name, "-in", "lammps.in",">","lammps.out"])
-#engine_proc = subprocess.Popen([engine_name, "-in", "lammps.in"])
-#driver_tup = driver_proc.communicate()
-#engine_proc.communicate()
-
 # Launch the driver in the background
-print("Launching the driver")
 driver_proc = subprocess.Popen([sys.executable, "min_driver.py", "-mdi", "-role DRIVER -name driver -method TCP -port 8021"],
                                stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd="./drivers")
 
 # Run LAMMPS as an engine
 os.system("cp -r " + str(working_dir) + " _work")
 os.chdir("./_work")
-print("Launching the engine")
 os.system("${USER_PATH}/lammps/src/lmp_mdi -mdi \"-role ENGINE -name TESTCODE -method TCP -port 8021 -hostname localhost\" -in lammps.in > lammps.out")
 
+# Convert the driver's output into a string
 driver_tup = driver_proc.communicate()
+driver_out = format_return(driver_tup[0])
+driver_err = format_return(driver_tup[1])
 
+print("Driver output: ")
+print(str(driver_out))
 
-print("Hello World!")
+print("Driver error message: ")
+print(str(driver_err))
