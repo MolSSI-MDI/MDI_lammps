66e8f596d43bfa2819933850fd0d2db04b916d78
taylor-a-barnes <taylor.a.barnes@gmail.com>
2020-06-16 16:49:14 -0400

    Update check_mdi_commands.py

--- .travis/codes/check_mdi_commands.py
+++ .travis/codes/check_mdi_commands.py
@@ -1,12 +1,35 @@
+import os
 import yaml
 
 def insert_list( original_list, insert_list, pos ):
     for ielement in range(len(insert_list)):
         element = insert_list[ielement]
         original_list.insert( pos + ielement + 1, element )
-        
+
+n_tested_commands = 0
 def test_command( command ):
-    return False
+    global n_tested_commands
+
+    driver_proc = subprocess.Popen([sys.executable, "min_driver.py", "-command", "<NAME", "-nreceive", "MDI_NAME_LENGTH", "-rtype", "MDI_CHAR", "-mdi", "-role DRIVER -name driver -method TCP -port 8021"],
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd="./drivers")
+    
+    # Run LAMMPS as an engine
+    working_dir = "../../user/mdi_tests/test1"
+    os.system("rm -rf ./_work")
+    os.system("cp -r " + str(working_dir) + " _work")
+    os.chdir("./_work")
+    os.system("${USER_PATH}/lammps/src/lmp_mdi -mdi \"-role ENGINE -name TESTCODE -method TCP -port 8021 -hostname localhost\" -in lammps.in > lammps.out")
+    os.chdir("../")
+
+    # Convert the driver's output into a string
+    driver_tup = driver_proc.communicate()
+    driver_out = format_return(driver_tup[0])
+    driver_err = format_return(driver_tup[1])
+
+    if driver_err == "":
+        return True
+    else:
+        return False
 
 def write_supported_commands():
     # List of all commands in the MDI Standard
