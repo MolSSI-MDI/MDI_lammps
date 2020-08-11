98accf62722d2604b03ea2c2d312f63804281d5b
taylor-a-barnes <taylor.a.barnes@gmail.com>
2020-08-11 12:54:55 -0400

    Update validate_build.sh

--- user/validate_build.sh
+++ user/validate_build.sh
@@ -1,5 +1,6 @@
-ENGINE_EXECUTABLE=./lammps/src/lmp_mdi
-if test -f "$ENGINE_EXECUTABLE"; then
+#ENGINE_EXECUTABLE=./lammps/src/lmp_mdi
+ENGINE_EXECUTABLE=/docker_image/lammps/src/lmp_mdi
+if docker run --rm travis/mdi_test test -f "$ENGINE_EXECUTABLE"; then
     echo "$ENGINE_EXECUTABLE exists"
 else
     echo "Could not find engine executable: $ENGINE_EXECUTABLE"
