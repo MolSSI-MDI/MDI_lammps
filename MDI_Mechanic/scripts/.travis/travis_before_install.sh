e43c6a0434e932bc5da3934535bda627613501d1
taylor-a-barnes <taylor.a.barnes@gmail.com>
2020-08-21 10:04:13 -0400

    Update travis_before_install.sh

--- MDI_Mechanic/scripts/.travis/travis_before_install.sh
+++ MDI_Mechanic/scripts/.travis/travis_before_install.sh
@@ -9,6 +9,7 @@ sudo apt-get update
 # Install conda
 wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
+source "$HOME/miniconda/etc/profile.d/conda.sh"
 export PATH=$HOME/miniconda/bin:$PATH
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
