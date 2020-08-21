b936d303ff25491fcc9f4058028d91fb8a6ab11a
taylor-a-barnes <taylor.a.barnes@gmail.com>
2020-08-21 11:57:24 -0400

    Update travis_script.sh

--- MDI_Mechanic/scripts/.travis/travis_script.sh
+++ MDI_Mechanic/scripts/.travis/travis_script.sh
@@ -5,4 +5,16 @@ set -e
 
 source "$HOME/miniconda/etc/profile.d/conda.sh"
 conda activate base
-./MDI_Mechanic/scripts/.internal/tutorial.sh
+
+# Run the script to generate the MDI report
+#./MDI_Mechanic/scripts/.internal/tutorial.sh
+if ./MDI_Mechanic/scripts/.internal/tutorial.sh ; then
+    echo "Report script succeeded."
+    export MDI_REPORT_STATUS=0
+else
+    echo "Report script failed."
+    export MDI_REPORT_STATUS=1
+fi
+
+# Push any changes to the report
+./MDI_Mechanic/scripts/.internal/push_changes.sh
