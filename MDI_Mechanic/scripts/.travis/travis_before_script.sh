752f5cf5bc7fa533c25fac6a70259715cb0cabbf
taylor-a-barnes <taylor.a.barnes@gmail.com>
2020-08-21 13:26:40 -0400

    Add dynamic badges to base report

--- MDI_Mechanic/scripts/.travis/travis_before_script.sh
+++ MDI_Mechanic/scripts/.travis/travis_before_script.sh
@@ -25,4 +25,4 @@ git remote -v
 git push -v > /dev/null 2>&1
 
 # Add the configuration success badge to the report
-cp ./MDI_Mechanic/badges/-working-success.svg ./report/dynamic_badges/step_config.svg
+#cp ./MDI_Mechanic/badges/-working-success.svg ./report/dynamic_badges/step_config.svg
