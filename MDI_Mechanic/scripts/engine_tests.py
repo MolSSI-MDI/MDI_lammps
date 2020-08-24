fd253e6a6c75bd3e2ff125df8848e7c8f8239750
taylor-a-barnes <taylor.a.barnes@gmail.com>
2020-08-24 14:04:56 -0400

    Update engine_tests.py

--- MDI_Mechanic/scripts/engine_tests.py
+++ MDI_Mechanic/scripts/engine_tests.py
@@ -2,13 +2,15 @@ import os
 
 # Change directory to this file's location
 file_path = os.path.dirname(os.path.realpath(__file__))
-os.chdir(file_path)
 
-# Construct the absolute path to the repo's top-level directory
-top_dir = os.getcwd() + "/../../"
+# Path to the top-level directory
+base_path = file_path + "/../.."
 
 # Run the engine test script
-bash_command = "docker run --net=host --rm -v " + str(top_dir) + ":/repo -it travis/mdi_test bash -c \"cd /repo/user/engine_tests/test1 && ./run.sh\""
+working_dir = str(base_path) + "/user/engine_tests/test1"
+os.system("rm -rf " + str(base_path) + "/.work")
+os.system("cp -r " + str(working_dir) + " " + str(base_path) + "/.work")
+bash_command = "docker run --net=host --rm -v " + str(base_path) + ":/repo -it travis/mdi_test bash -c \"cd /repo/.work && ./run.sh\""
 ret = os.system(bash_command)
 if ret != 0:
     raise Exception("Engine test script returned non-zero value.")
