551d4f36e216ff2874daf2ff86b070fcd211a5e8
taylor-a-barnes <taylor.a.barnes@gmail.com>
2020-08-25 10:32:13 -0400

    Move location of .work directories

--- MDI_Mechanic/scripts/engine_tests.py
+++ MDI_Mechanic/scripts/engine_tests.py
@@ -8,9 +8,9 @@ base_path = file_path + "/../.."
 
 # Run the engine test script
 working_dir = str(base_path) + "/user/engine_tests/test1"
-os.system("rm -rf " + str(base_path) + "/.work")
-os.system("cp -r " + str(working_dir) + " " + str(base_path) + "/.work")
-bash_command = "docker run --net=host --rm -v " + str(base_path) + ":/repo -it travis/mdi_test bash -c \"cd /repo/.work && ./run.sh\""
+os.system("rm -rf " + str(base_path) + "/user/engine_tests/.work")
+os.system("cp -r " + str(working_dir) + " " + str(base_path) + "/user/engine_tests/.work")
+bash_command = "docker run --net=host --rm -v " + str(base_path) + ":/repo -it travis/mdi_test bash -c \"cd /repo/user/engine_tests/.work && ./run.sh\""
 ret = os.system(bash_command)
 if ret != 0:
     raise Exception("Engine test script returned non-zero value.")
--- MDI_Mechanic/scripts/unsupported_test.py
+++ MDI_Mechanic/scripts/unsupported_test.py
@@ -34,9 +34,9 @@ driver_proc = subprocess.Popen([sys.executable, "min_driver.py", "-command", "UN
 # Run the engine, using Docker
 mdi_engine_options = "-role ENGINE -name TESTCODE -method TCP -hostname " + str(hostname) + " -port 8021"
 working_dir = str(base_path) + "/user/mdi_tests/test1"
-os.system("rm -rf " + str(base_path) + "/.work")
-os.system("cp -r " + str(working_dir) + " " + str(base_path) + "/.work")
-docker_string = "docker run --net=host --rm -v " + str(base_path) + ":/repo -it travis/mdi_test bash -c \"cd /repo/.work && ls && export MDI_OPTIONS=\'" + str(mdi_engine_options) + "\' && ./run.sh\""
+os.system("rm -rf " + str(base_path) + "/user/mdi_tests/.work")
+os.system("cp -r " + str(working_dir) + " " + str(base_path) + "/user/mdi_tests/.work")
+docker_string = "docker run --net=host --rm -v " + str(base_path) + ":/repo -it travis/mdi_test bash -c \"cd /repo/user/mdi_tests/.work && ls && export MDI_OPTIONS=\'" + str(mdi_engine_options) + "\' && ./run.sh\""
 os.system(docker_string)
 
 # Convert the driver's output into a string
