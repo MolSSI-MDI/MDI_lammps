a38c22427047c8e88e1b70f55c883daf9741cce9
taylor-a-barnes <taylor.a.barnes@gmail.com>
2020-09-01 11:34:11 -0400

    Update travis scripts

--- MDI_Mechanic/scripts/.travis/travis_before_install.sh
+++ MDI_Mechanic/scripts/.travis/travis_before_install.sh
@@ -4,7 +4,24 @@
 set -e
 
 # Set Travis CI badge
-echo "Web URL: ${TRAVIS_BUILD_WEB_URL}"
+echo "[![Build Status](${TRAVIS_BUILD_WEB_URL%/builds*}.svg?branch=master)](${TRAVIS_BUILD_WEB_URL%/builds*})"
+
+# Write out the MDI key
+echo "-----BEGIN OPENSSH PRIVATE KEY-----" > travis_key
+echo ${mdi_key} >> travis_key
+echo "-----END OPENSSH PRIVATE KEY-----" >> travis_key
+echo "Host github.com" > ~/.ssh/config
+echo "  IdentityFile  $(pwd)/travis_key" >> ~/.ssh/config
+chmod 400 travis_key
+
+# Configure Travis to use Git
+git config --global user.email "travis@travis-ci.com"
+git config --global user.name "Travis CI"
+
+# Switch the remote to use ssh
+git remote -v
+git remote set-url origin git@github.com:MolSSI-MDI/MDI_lammps2.git
+git checkout ${TRAVIS_BRANCH}
 
 # Update apt-get
 sudo apt-get update
