aa9df72198e1b2e62a4e18f87e45bb2d1f3592eb
taylor-a-barnes <taylor.a.barnes@gmail.com>
2020-09-09 13:24:43 -0400

    Use subprocess in check_mdi_nodes.py

--- MDI_Mechanic/scripts/utils/check_mdi_nodes.py
+++ MDI_Mechanic/scripts/utils/check_mdi_nodes.py
@@ -1,5 +1,5 @@
 import os
-import sys
+import subprocess
 import yaml
 from graphviz import Digraph
 
@@ -33,15 +33,11 @@ def test_command( command, nrecv, recv_type, nsend, send_type ):
     #print("Starting min_driver.py with command: " + str(command))
     
     # Remove any leftover files from previous runs of min_driver.py
-    #os.system("rm ./drivers/min_driver.dat")
-    #os.system("rm ./drivers/min_driver.err")
     if os.path.exists(str(base_path) + "/MDI_Mechanic/scripts/drivers/min_driver.dat"):
         os.remove(str(base_path) + "/MDI_Mechanic/scripts/drivers/min_driver.dat")
     if os.path.exists(str(base_path) + "/MDI_Mechanic/scripts/drivers/min_driver.err"):
         os.remove(str(base_path) + "/MDI_Mechanic/scripts/drivers/min_driver.err")
 
-    port_num = 9050 + n_tested_commands
-    #mdi_driver_options = "-role DRIVER -name driver -method TCP -port " + str(port_num)
     mdi_driver_options = "-role DRIVER -name driver -method TCP -port 8021"
 
     # Create the docker script
@@ -68,16 +64,31 @@ def test_command( command, nrecv, recv_type, nsend, send_type ):
     with open(docker_file, 'w') as file:
         file.writelines( docker_lines )
 
-    # Run the driver and engine, using Docker Compose
-    os.chdir(str(base_path) + "/MDI_Mechanic/docker")
-    ret = os.system("docker-compose up --exit-code-from mdi_mechanic --abort-on-container-exit")
-    if ret != 0:
+    docker_path = os.path.join( base_path, "MDI_Mechanic", "docker" )
+
+    # Run "docker-compose up"
+    up_proc = subprocess.Popen( ["docker-compose", "up", "--exit-code-from", "mdi_mechanic", "--abort-on-container-exit"],
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                cwd=docker_path )
+    up_tup = up_proc.communicate()
+    up_out = format_return(up_tup[0])
+    up_err = format_return(up_tup[1])
+    if up_proc.returncode != 0:
         print("FAILED")
         return False
-    ret = os.system("docker-compose down")
-    if ret != 0:
+
+    # Run "docker-compose down"
+    down_proc = subprocess.Popen( ["docker-compose", "down"],
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                cwd=docker_path )
+    down_tup = down_proc.communicate()
+    down_out = format_return(down_tup[0])
+    down_err = format_return(down_tup[1])
+    if down_proc.returncode != 0:
         print("FAILED")
         return False
+
+
     print("WORKED")
     return True
 
@@ -206,7 +217,6 @@ def write_supported_commands():
         line = "| " + str(command) + " "
         for node in node_paths.keys():
             command_with_path = node_paths[node] + " " + command
-            #print("FFF: " + str(command_with_path), end=" ")
             padded_string = str(node).ljust(20, '.')
             print(padded_string, end=" ")
             command_works = test_command( command_with_path, nrecv, recv_type, nsend, send_type )
@@ -235,27 +245,12 @@ def node_graph():
     global node_edge_paths
 
     dot = Digraph(comment='Node Report', format='svg')
-    #dot.node('@DEFAULT', '@DEFAULT')
-    #dot.node('@INIT_MD', '@INIT_MD')
-    #dot.node('@INIT_OPTG', '@INIT_OPTG')
-    #dot.node('@INIT_MC', '@INIT_MC')
-    #dot.node('@INIT_MD_', '@PRE-FORCES\n@FORCES\n@COORDS')
-    #dot.node('@INIT_OPTG_', '@PRE-FORCES\n@FORCES\n@COORDS')
-
-    #dot.edge('@DEFAULT', '@INIT_MD')
-    #dot.edge('@DEFAULT', '@INIT_OPTG')
-    #dot.edge('@DEFAULT', '@INIT_MC')
-    #dot.edge('@INIT_MD', '@INIT_MD_')
-    #dot.edge('@INIT_OPTG', '@INIT_OPTG_')
 
     print("*********************************************")
     print("* Creating node graph                       *")
     print("*********************************************")
 
     print("node_edge_paths: " + str(node_edge_paths))
-
-    #for node_name in node_paths.keys():
-    #    dot.node( node_name, node_name )
 
     nodes = {}
     edges = []
@@ -280,20 +275,6 @@ def node_graph():
 
     for edge in edges:
         dot.edge( edge[0], edge[1] )
-    
-    #for edge_path in node_edge_paths:
-    #    print("edge_path: " + str(edge_path))
-    #    name = edge_path[0]
-    #    path = edge_path[1]
-    #    dot.node( name, name )
-
-    #    # Add edges for the nodes that are directly accessed from @DEFAULT
-    #    if name == path:
-    #        dot.edge('@DEFAULT', name)
-    #    elif path.find(' @ ') != -1 or path[-2:] == ' @':
-    #        split_path = path.split()
-    #        parent = split_path[0]
-    #        dot.edge(parent, name)
 
     dot.render(str(base_path) + '/report/graphs/node-report.gv')
 
