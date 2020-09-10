dd1e7fd82404fd2552aa73484005986304f97ebf
taylor-a-barnes <taylor.a.barnes@gmail.com>
2020-09-10 16:54:25 -0400

    Update install process

--- MDI_Mechanic/scripts/install_engine.py
+++ MDI_Mechanic/scripts/install_engine.py
@@ -4,11 +4,10 @@ import yaml
 
 # Path to this file
 file_path = os.path.dirname(os.path.realpath(__file__))
+base_path = os.path.dirname( os.path.dirname( file_path ) )
 
-# Path to the top-level directory
-base_path = file_path + "/../.."
-
-
+# Build the docker image
+#os.system("docker build -t travis/mdi_test user")
 
 # Run the engine, using Docker
 docker_string = "docker run --rm -v " + str(base_path) + ":/repo -it travis/mdi_test bash -c \"cd /repo/user && ls && ./docker_install.sh\""
--- user/docker_install.sh
+++ user/docker_install.sh
@@ -1,27 +1,36 @@
 # Obtain a clone of LAMMPS
-git clone https://github.com/MolSSI-MDI/lammps.git build/lammps
+if [ ! -d "build/lammps" ]; then
+    git clone https://github.com/MolSSI-MDI/lammps.git --branch mdi build/lammps
+fi
 LAMMPS_INSTALL='serial'
 #LAMMPS_INSTALL='mpi'
 
 # Configure LAMMPS
-cd build/lammps
-git checkout mdi
-cd src
-make yes-standard
-make no-gpu
-make no-kim
-make no-kokkos
-#make no-kspace
-make no-latte
-make no-meam
-make no-mpiio
-make no-mscg
-make no-poems
-make no-python
-make no-reax
-make no-voronoi
-make no-user-qmmm
+cd build/lammps/src
+make yes-asphere
+make yes-body
+make yes-class2
+make yes-colloid
+make yes-compress
+make yes-coreshell
+make yes-dipole
+make yes-granular
+make yes-kspace
+make yes-manybody
+make yes-mc
+make yes-misc
+make yes-molecule
+make yes-opt
+make yes-peri
+make yes-qeq
+make yes-replica
+make yes-rigid
+make yes-shock
+make yes-snap
+make yes-srd
 make yes-user-mdi
+
+# Build the MDI Library
 cd ../lib/mdi
 #python Install.py -m gcc
 make -f Makefile.gcc
