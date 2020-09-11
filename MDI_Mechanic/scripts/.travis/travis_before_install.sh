6adfed58e7a95d9e6ee0a7f9e40173f542fa27be
taylor-a-barnes <taylor.a.barnes@gmail.com>
2020-09-11 13:41:19 -0400

    Update travis_before_install.sh

--- MDI_Mechanic/scripts/.travis/travis_before_install.sh
+++ MDI_Mechanic/scripts/.travis/travis_before_install.sh
@@ -49,18 +49,18 @@ install_dependencies() {
     sudo apt-get update
 
     # Install conda
-    wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-    bash miniconda.sh -b -p $HOME/miniconda
-    source "$HOME/miniconda/etc/profile.d/conda.sh"
-    export PATH=$HOME/miniconda/bin:$PATH
-    conda config --set always_yes yes --set changeps1 no
-    conda update -q conda
-    conda info -a
+    #wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+    #bash miniconda.sh -b -p $HOME/miniconda
+    #source "$HOME/miniconda/etc/profile.d/conda.sh"
+    #export PATH=$HOME/miniconda/bin:$PATH
+    #conda config --set always_yes yes --set changeps1 no
+    #conda update -q conda
+    #conda info -a
 
     # Install pymdi
-    conda activate base
-    conda install -c conda-forge pymdi pyyaml pandas python-graphviz
-    conda info -a
+    #conda activate base
+    #conda install -c conda-forge pymdi pyyaml pandas python-graphviz
+    #conda info -a
 
     # Create the MDI_Mechanic docker image
     #python MDI_Mechanic/scripts/install_mechanic.py
