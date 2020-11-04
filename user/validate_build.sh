2206e93f2df73ba6e19e2e0f6c279fa3d79b1470
taylor-a-barnes <taylor.a.barnes@gmail.com>
2020-11-04 17:06:36 -0500

    Add validate_engine yaml script

--- user/validate_build.sh
+++
@@ -1,9 +0,0 @@
-#ENGINE_EXECUTABLE=./lammps/src/lmp_mdi
-#ENGINE_EXECUTABLE=/docker_image/lammps/src/lmp_mdi
-ENGINE_EXECUTABLE=/repo/user/build/lammps/src/lmp_mdi
-if test -f "$ENGINE_EXECUTABLE"; then
-    echo "$ENGINE_EXECUTABLE exists"
-else
-    echo "Could not find engine executable: $ENGINE_EXECUTABLE"
-    exit 1
-fi
